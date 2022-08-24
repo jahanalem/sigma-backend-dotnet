b30b67c996dd7269707b08a2e4d5e9f72c425a83
Said Roohullah Allem <s.r.alem19@gmail.com>
2022-08-24 22:37:56 +0200

    added validation

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -14,7 +14,11 @@ namespace API.Helpers
                 .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
                 .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
 
-            CreateMap<Address, AddressDto>().ReverseMap();   
+            CreateMap<Address, AddressDto>().ReverseMap();
+
+            CreateMap<CustomerBasketDto, CustomerBasket>();
+            
+            CreateMap<BasketItemDto, BasketItem>();
         }
     }
 }
